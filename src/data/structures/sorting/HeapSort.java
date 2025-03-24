a97f188ca8ab05cadc21f4eec3de94bbf24d68e2
Swilam <mahmoudswilam24@gmail.com>
2025-03-24 09:11:11 +0200

    update

--- src/data/structures/sorting/HeapSort.java
+++ src/data/structures/sorting/HeapSort.java
@@ -8,6 +8,83 @@ package data.structures.sorting;
  *
  * @author Swilam
  */
+
+/*
+    بيشتغل بطريقة ال
+    binary tree
+    بس مش بيشرط عليك تكون ان الطفل الاول اكبر من التاني
+    في الاول بتاخد الليستة وتعملها 
+    heapify
+    عشان تكون
+    binary tree
+    وبعدع تاخد اصغر عنصر الي هو ال 
+    root
+    ويكون اكبر عنصر فيهم وتبدله من مكانه بحيث يكون في اخر الليستة
+    وتعد تلف علي الباقي بنفس الطريقة
+
+*/
+
 public class HeapSort {
     
+    //دي نقطة البداية هنا بتديله الليستة
+    
+     public  void heapSort(int[] array) {
+         //هنا بنخزن طول الليستة
+        int n = array.length;
+
+        // هنا هنفضل ماشيين علي النص الاول من العناصر
+        // مش كل الليستة لان النص الاول هنشتغل عليه ك 
+        //parent
+        // ونجيب ال 
+        //child
+        //بتوعه من باقي الليستة
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(array, n, i);
+        }
+
+        // هنا بعد اما يعملi
+        //binary tree
+        //بياخد العنصر الاكبر ويبدله مع اخر عنصر
+        //عشان يبد يرتب ويمشي علي بقية العناصر برضو لحدالعنصر الاول بيكون هو في مكانه خلاص
+        for (int i = n - 1; i > 0; i--) {
+           
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // بعد اما بندل العنصرين بنحتاج نحولها ل
+            //binary tree
+            //تاني عشان ناخد العنصر الاكبر
+            heapify(array, i, 0);
+        }
+    }
+
+    // هنا الدالة الي بتخلي عناصر الليستة
+     //heap
+     
+    private void heapify(int[] array, int n, int i) {
+        int largest = i; // بنخلي العنصر الاول ليه قيمة مبدئية
+        int left = 2 * i + 1; // طريقة حساب الطفل الشمال
+        int right = 2 * i + 2; // طريقة حساب الطفي اليمين
+
+        // هنا لو كان الطفل الشمال اكبر من الاب بنخلي العنصر الكبير بيشاور علي الشمال
+        if (left < n && array[left] > array[largest]) {
+            largest = left;
+        }
+
+        // وهنا لو كان الابن اليمين هو الاكبر
+        if (right < n && array[right] > array[largest]) {
+            largest = right;
+        }
+
+        // هنا لو لقي ان في ابن اكبر من الاب بيبدل بينهم
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            // ونعيد اللفة تاني عشان يبقي كله مظبوط
+            heapify(array, n, largest);
+        }
+    }
 }
--- src/data/structures/sorting/QuickSort.java
+++ src/data/structures/sorting/QuickSort.java
@@ -8,33 +8,70 @@ package data.structures.sorting;
  *
  * @author Swilam
  */
+
+/*
+    بيشتغل بانك بتاخد اي عنصر وليكن الاول مثلا
+    وبعد كده بفضل تعمل تبديل مع باقي العناصر
+    بحيث انه يفضل يبدل مع العناصر ال الباقية ويخلي كل العناصر الي اصغر منه علي شماله
+    والاكبر منه علي يمينه
+    وتقسم الليستة نصين بناء علي العنصر الي رتبته وبقي في مكانه خلاص والي علي شماله اصغر منه والي علي اليمين اكبر
+    لحد ما توصل ان كله مترتب
+
+    best case space complexity: O(log(n)) 
+    worst case space complexity: O(n) 
+    best case time complexity: O(n log(n)) 
+    worst case time complexity: O(n^2) 
+    divide and conquer algorithm
+*/
+
 public class QuickSort {
+    
+    //هنا نقطة البداية بتديله هو هيبدا من انهي عنصر ولحد فين عشان في
+    //ٌRecustion
      public void quick_sort(int[] arr,int start,int end){
        
+         //base case
+         //ان يفضل يقسم لحد اما يكون نقطة البداية اصغر من النهاية
+         //يعني ميبقاش في ليستة من عنصر واحد
        if(start < end){
-                  int pivot_location = partioning(arr,start,end);
-
+           
+           //هنا بتجيب العنصر الي هترتبه عشان تخلي الصغير علي الشمال ةالكبير علي اليمين
+            int pivot_location = partioning(arr,start,end);
+            
+            //هنا بتقسم الليستة الي علي اليمين والشمال وتفضل تقسم برضو
            quick_sort(arr,start,pivot_location-1);
-       quick_sort(arr,pivot_location+1,end);
+           quick_sort(arr,pivot_location+1,end);
        }
    }
    
+     //هنا الدالة الي بتجيب العنصر وترتبه وتخليه في مكانه الصحيح
    private int partioning(int[] arr,int start,int end){
+       
+       //هناخد عشوائي العنصر الاول يكون الي هنرتبه
        int pivot_location = start;
        
+       //دي اللوب الكبيرة عشان لو كان العنصر في مكانه الصحيح فعلا نخليه يخرج وميعملش حاجة
        while(true){
+           //هنا هنفضل نقارنه بكل الي علي اليمين لحد اما يلاقي في عنصر اصغر منه فعلا
+           //وهنا هنوقف في حالة انه لف علي كل الي علي اليمين لحد اما يلاقي عنصر اصغر
+           //او ميلاقيش ويكون عدي علي كل العنصار التانية
            while(arr[pivot_location] <= arr[end] && pivot_location != end){
                end--;
            }
+           
+           //هنا لو عدي علي كل العناصر وملاقش عنصر اصغر فبنخليه يقف لانه كده في مكانه الصحيح
            if(pivot_location == end){
                break;
            }
+           
+           //هنا لو لقي عنصر علي يمينه اصغر منه فعلا بنعمل تبديل بينهم ونكرر اللفة تاني عشان نرتبه تاني
            else if(arr[pivot_location] > arr[end]){
                int temp = arr[pivot_location];
                arr[pivot_location] = arr[end];
                arr[end] = temp;
            }
            
+           //ونكرر الكلام ده برضو علي الي علي الشمال عشان نخلي الي علي الشمال اصغر منه كله
            while(arr[pivot_location] >= arr[start] && pivot_location != start){
                start++;
            }

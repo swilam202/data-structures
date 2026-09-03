dfc727ddf0fddfe9b395dbe6b227a92638edcc37
swilam <m.swilam@storeus.com>
2026-09-03 13:50:25 +0300

    head and tail recursion

---
+++ new_ds/new_ds/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  new_ds
+//
+//  Created by Swilam on 03/09/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
